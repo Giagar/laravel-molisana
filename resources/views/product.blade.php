43f4da2cdbb6fe064785f2ab66c82e460165e755
Gianpaolo <pao.barga@gmail.com>
2021-03-12 09:14:38 +0100

    aggiunto route

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -185,105 +185,19 @@
     </head>
     <body>
 
-      <div class="wrapper">
-        
-        <header>
-          <div class="logo-container">
-            <img src="{{ asset('img/logo.png') }}" alt="logo" class="logo">
-          </div>
-          <nav class="header-nav">
-            <ul>
-              @foreach ($headerNavMenu as $itemNav)
-                <li>{{$itemNav}}</li>
-              @endforeach
-            </ul>
-          </nav>
-        </header>
-  
-        <div class="main-container">
-          <main>
+        <div class="wrapper">
+            questa è la pagina prodotto {{ $idProduct }}
+
+        {{ $array[$idProduct]['titolo'] }}
+        {{ $array[$idProduct]['tipo'] }}
+        {{ $array[$idProduct]['cottura'] }}
+        {{ $array[$idProduct]['peso'] }}
+        {{ $array[$idProduct]['descrizione'] }}
+
             
-            <section class="section paste-lunghe">
-              <h2>LE LUNGHE</h2>
-              <div class="pasta-container">
-                @foreach ($array as $key=>$itemPastaLunga)
-                  @if ($itemPastaLunga["tipo"] === "lunga")
-                    <div class="pasta-item">
-                      <a href="/product/{{$key}}">
-                        <img class="pasta-img" src="{{$itemPastaLunga['src']}}" alt="#">
-                      </a>
-                    </div>    
-                  @endif
-                @endforeach
-              </div>
-            </section>
+        
 
-            <section class="section paste-corte">
-              <h2>LE CORTE</h2>
-              <div class="pasta-container">
-                @foreach ($array as $key=>$itemPastaCorta)
-                  @if ($itemPastaCorta["tipo"] === "corta")
-                    <div class="pasta-item">
-                      <a href="/product/{{$key}}">
-                        <img class="pasta-img" src="{{$itemPastaCorta['src']}}" alt="#">
-                      </a>
-                    </div>    
-                  @endif
-                @endforeach
-              </div>
-            </section>
-
-            <section class="section paste-cortissime">
-              <h2>LE  CORTISSIME</h2>
-              <div class="pasta-container">
-                @foreach ($array as $key=>$itemPastaCortissima)
-                  @if ($itemPastaCortissima["tipo"] === "cortissima")
-                    <div class="pasta-item">
-                      <a href="/product/{{$key}}">
-                        <img class="pasta-img" src="{{$itemPastaCortissima['src']}}" alt="#">
-                      </a>
-                    </div>    
-                  @endif
-                @endforeach
-              </div>
-            </section>
-  
-          </main>
-        </div>
-  
-        <div class="footer-container">
-          <footer>
-            <div class="section-left">
-              <div class="logo-container">
-                <img src="" alt="logo" class="logo">
-              </div>
-              <div class="contacts">
-                <ul>
-                  @foreach ($contacts as $contact)
-                      <li>{{$contact}}</li>
-                  @endforeach
-                </ul>
-              </div>
-            </div>
-            <div class="section-right">
-              @foreach ($footerNav as $footerNavTitle=>$footerNavContent)
-                <h3>{{$footerNavTitle}}</h3>
-                <ul></ul>
-                  @foreach ($footerNavContent as $footerNavItem)
-                      <li>{{$footerNavItem}}</li>
-                  @endforeach
-                  
-              @endforeach
-            </div>
-          </footer>
         </div>
 
-      </div>
-
-
-        
-       
-
-        
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+
+Route::get('/product/{id}', function($id) {
+    return view('product',
+        ['idProduct' => $id]
+    );
+});
+
+
