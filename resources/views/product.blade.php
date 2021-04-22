9c4208c180e88f7bc192946aca45f6af58721438
Gianpaolo <pao.barga@gmail.com>
2021-04-23 01:46:30 +0200

    sistemato ol ul

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('main')
-  
+
   <main class="main-container-product">
     {{-- dai seguenti indici tolgo 1 perché l'ho aggiunto nei link delle rispettive paste in home per prevenire problemi con verifica empty in route --}}
       <h1>{{ $pastaArray[$idProduct - 1]['titolo'] }}</h1>
@@ -9,7 +9,8 @@
         <img class="pasta-top" src="{{$pastaArray[$idProduct - 1]['src-h']}}" alt="immagine pasta">
         <img class="pasta-bottom" src="{{$pastaArray[$idProduct - 1]['src-p']}}" alt="immagine pasta">
       </div>
-      <p class="description">{!! $pastaArray[$idProduct - 1]['descrizione'] !!}</p>
+      {{-- per stampare come html e non come stringa: {!! !!} --}}
+      <div class="description"><p class="description">{!! $pastaArray[$idProduct - 1]['descrizione'] !!}</p></div>
   </main>
-    
+
 @endsection
